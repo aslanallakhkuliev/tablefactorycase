9d8d5b4cb4ec1ec4a528d0ca3474e05e4209c6bc
Aslan Allakhkuliev <aslan.allakhkuliev@gmail.com>
2021-03-04 15:28:03 -0600

    modified:   tables/admin.py

--- tables/admin.py
+++ tables/admin.py
@@ -5,7 +5,7 @@ class TableAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
 class LegAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'table_id')
 
 class FootAdmin(admin.ModelAdmin):
     list_display = ('id', 'width', 'length', 'radius', 'legs_list') 
@@ -13,5 +13,5 @@ class FootAdmin(admin.ModelAdmin):
         return ", ".join([str(p.id) for p in obj.legs.all()])
 
 admin.site.register(Table, TableAdmin)
-admin.site.register(Leg)
+admin.site.register(Leg, LegAdmin)
 admin.site.register(Foot, FootAdmin)
